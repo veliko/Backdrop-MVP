be6ba2ca401dc6c35f42db233f14099ef9d5b634
veliko <vcm060@gmail.com>
2016-02-16 13:29:34 -0800

    Style ambience control bar on the bottom of page.

--- app/sc_widget/youTube.js
+++ app/sc_widget/youTube.js
@@ -18,22 +18,32 @@ function onYouTubeIframeAPIReady() {
 
 function initialize(){
   window.player.setVolume(10);
+  
   $("#up").on('click', function(){
     player.setVolume(player.getVolume()+5);
   });
+  
   $("#down").on('click', function(){
     player.setVolume(player.getVolume()-5);
   });
+  
   $("#play").on('click', function(){
     player.playVideo();
   });
+  
   $("#pause").on('click', function(){
     player.pauseVideo();
   });
-  $("#coruscant").on('click', function(){
-    player.loadVideoById('D24_zcoiTj0');
-  });
-  $("#dromund").on('click', function(){
-    player.loadVideoById('a96_r--pOxE');
+  
+  $("select").on('change', function(){
+    var option = $( "select option:selected" ).text();
+    console.log(option);
+    switch (option) {
+      case "Star Wars: Coruscant Penthouse": player.loadVideoById('D24_zcoiTj0'); break;
+      case "Sith Imperial Capital Dromund Kaas": player.loadVideoById('a96_r--pOxE'); break;
+      case "Heavy Rain": player.loadVideoById('V9Xu6mkacg8'); break;
+      case "Thunderstorm": player.loadVideoById('t3v4ZO9iTg8'); break;
+      case "Crackling Fire": player.loadVideoById('4gSrHPBMC8w'); break;
+    }
   });
 }
